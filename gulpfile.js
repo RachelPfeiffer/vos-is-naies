4297ba3b9dbcec25c24aadbf66fbd44ea663bef2
RachelPfeiffer <42552533+RachelPfeiffer@users.noreply.github.com>
2019-01-28 22:22:33 +0200

    1 day project - vos is naies

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,23 @@
-// putting in some text so github will let me use it.
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("app/scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("app/css"))
+        .pipe(browserSync.stream({match: '**/*.css'}));
+});
+
+browserSync.init({
+          server: "app",
+          injectChanges: true
+      });
+
+// Static Server + watching scss/html files
+gulp.task('watch', function() {
+    gulp.watch('app/scss/*.scss', gulp.series('sass'));
+    gulp.watch('app/*.html').on('change', browserSync.reload);
+    // gulp.watch("app/*.html", browserSync.reload);
+});
